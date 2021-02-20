b2c16fb1a7a0f4a0e11325748e1e6813a4a378d3
scriptgr <79371909+scriptgr@users.noreply.github.com>
2021-02-20 21:22:43 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMessageBox, QAction, qApp, QMenu, QApplication, QDialog, QSystemTrayIcon, QStyle
+
+from vk_api import VkApi
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.utils import get_random_id
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject
+import threading 
+from processor import Processor
+import threading
+import init
+import sys  
+import os
+import untitled
+
+
+class ExampleApp(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
+   
+    def __init__(self):    # Main Processor ui class...
+        super().__init__()
+        
+        self.setupUi(self)
+        self.index = 0 
+        self.sex = 0    
+        self.number = self.spin_number_01.value() 
+        header = self.tableWidget.horizontalHeader()    
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        self.tableWidget.setRowCount(self.number)
+
+        self.btn_send.clicked.connect(self.hunter)
+        self.btn_sear.clicked.connect(self.start)
+        
+        self.check_box_01.clicked.connect(self.check_01)
+        self.check_box_02.clicked.connect(self.check_02)
+        
+        self.radio_01.toggled.connect(self.radio_clicked)
+        self.radio_02.toggled.connect(self.radio_clicked)
+        self.radio_03.toggled.connect(self.radio_clicked)
+        
+        self.spin_number_01.lineEdit().setReadOnly(True)
+        self.spin_number_01.valueChanged.connect(self.update_number)
+        
+    def update_number(self):
+        self.number = self.spin_number_01.value() 
+
+
+
+    def radio_clicked(self):
+        radioButton = self.sender()
+        if radioButton.isChecked():
+            self.sex = radioButton.text()
+
+    def check_01(self): 
+        if self.check_box_01.isChecked():
+            return True
+        return False
+ 
+    def check_02(self): 
+        if self.check_box_02.isChecked():
+            return True
+        return False
+    
+    def check_03(self): 
+        if self.check_box_03.isChecked():
+            return True
+        return False
+
+
+    def hunter(self):
+        is_token = self.lineEdit_1.text() 
+        is_users = self.lineEdit_2.text() 
+        is_messa = self.txtMessage.toPlainText()
+
+        vk_session = VkApi(token = is_token)
+        vk = vk_session.get_api()
+        
+    
+        if vk is not None:
+            vk.messages.send(user_id = int(is_users), random_id = get_random_id(), message = is_messa)
+        else:
+           print("Собщение не отправлено")
+
+
+    def start(self):    # Start stream metod...
+        
+        self.index = 0 
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setRowCount(self.number)
+        
+        token = self.lineEdit_1.text() 
+        group = self.lineEdit_3.text()
+        stolder = self.spin_number_02.value()
+        edolder = self.spin_number_03.value()
+ 
+
+
+        self.th = Processor(token, group, self.number, stolder, edolder, self.sex, self.check_01(), self.check_02(), self.check_03())
+        self.th.length.connect(self.append)
+        threading.Thread(target = self.th.running).start()
+
+        self.statusBar.showMessage("Start processor...")
+        
+    
+
+
+    def append(self, add_id, add_first, add_last, add_date, add_online, add_dialog):    # Append stream metod...
+        
+        index = 0
+        self.index = self.index + 1
+        index = self.index - 1
+        
+        add_online = int(add_online)
+        if add_online == 0:
+            
+            add_active = "Офлайн"
+        else:
+            add_active = "Онлайн"
+        
+        add_dialog = int(add_dialog)
+        if add_dialog == 0:
+            
+            add_sender = "Закрыты"
+        else:
+            add_sender = "Открыты" 
+
+        
+        item = QtWidgets.QTableWidgetItem(add_id)
+        self.tableWidget.setItem(index, 0, item) 
+        
+        item = QtWidgets.QTableWidgetItem(add_first)
+        self.tableWidget.setItem(index, 1, item)               
+
+        item = QtWidgets.QTableWidgetItem(add_last)
+        self.tableWidget.setItem(index, 2, item)               
+
+        item_ative = QtWidgets.QTableWidgetItem(add_active)
+        self.tableWidget.setItem(index, 3, item_ative)               
+
+        item = QtWidgets.QTableWidgetItem(add_sender)
+        self.tableWidget.setItem(index, 4, item)   
+        
+        if add_online != 0:
+            item_ative.setForeground(QtGui.QColor(126, 224, 120))
+
+        self.statusBar.showMessage("Retrive user id " + add_id)
+        self.tableWidget.repaint()
+        self.tableWidget.scrollToItem(item)        
+
+     
+
+      
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # Запускаем приложение
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
---
+++ processor.py
@@ -0,0 +1,266 @@
+# -*- coding: utf-8 -*-
+from vk_api import VkApi
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.utils import get_random_id
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject
+import time
+
+import threading 
+from time import sleep
+
+class Processor(QObject):    # Основной класс...
+    
+    length = QtCore.pyqtSignal(str, str, str, str, str, str)
+    
+    def __init__(self, token, group, number, stolder, edolder, sex, dialgs, online, old):    # Основной класс...
+        super().__init__()
+        self.offset = 0
+        self.array = []
+        
+        self.token = token
+        self.group = group
+        self.number = number
+        self.stolder = stolder
+        self.edolder = edolder
+        self.sex = sex
+        self.dialgs = dialgs
+        self.online = online
+        self.old = old
+        print(self.sex)
+
+        self.vk_session = VkApi(token = self.token)
+        self.vk = self.vk_session.get_api()
+    
+    #---------------------------------------------------------------------------------------------
+    def running(self): #Метод запуска основного процесса поиска...
+    
+        while True: 
+            resp = self.vk.groups.getMembers(group_id = self.group, offset = self.offset, sort = "id_desc", fields = "bdate, sex, online, can_write_private_message")        
+            self.array += resp["items"]
+            self.offset += 1000
+            if self.offset > resp["count"]:
+                break
+            elif len(self.array) == self.number:
+                break
+    
+    
+        for lists in self.array:
+            self.sleep()
+            if self.dialgs == True and self.online == True and self.old == True:
+                self.on_old_open_dial(lists)
+                
+            elif self.dialgs == True and self.online == True:
+                self.on_open_dial(lists)
+           
+            elif self.dialgs == True and self.old == True:
+                self.old_dial(lists)
+
+            elif self.online == True and self.old == True:
+                self.on_old(lists)
+
+            elif self.old == True:
+                self.older(lists)
+            
+            elif self.dialgs == True:
+                self.open_dial(lists)
+                
+            elif self.online == True:
+                self.on(lists)
+                   
+            else:   
+                self.off(lists)
+                
+                
+    #---------------------------------------------------------------------------------------------
+    def sleep(self):    # Метод устанавливает задержку в мс...
+    
+        sleep = time.sleep(0.001)
+        return sleep
+
+
+
+    #---------------------------------------------------------------------------------------------
+    def older(self, lists):    # Метод поиска пользователей сообщества по возрасту
+        try: 
+            add_date = str(lists["bdate"])
+
+            if len(add_date) >= 4 and add_date[-4:].isdigit():
+                rez = add_date[-4:]
+                
+                if self.sex == "Женский":
+                    gender = lists["sex"]
+                    if gender == 1 and int(2021 - int(rez)) == self.stolder:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif self.sex == "Мужской":
+                    gender = lists["sex"]
+                    if gender == 2 and int(2021 - int(rez)) == self.stolder:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif int(2021 - int(rez)) == self.stolder:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+   
+        except Exception as error:
+            print(error)
+
+      
+    #---------------------------------------------------------------------------------------------
+    def off(self, lists):    # Метод поиска всех пользователей сообщества
+        try:
+            if self.sex == "Женский":
+                gender = lists["sex"]
+                if gender == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif self.sex == "Мужской":
+                gender = lists["sex"]
+                if gender == 2:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            else:
+                self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+        
+        except Exception as error:
+            print(error)
+
+
+    #---------------------------------------------------------------------------------------------
+    def on(self, lists):     # Метод поиска пользователей сообщества онлайн
+        try: 
+            if self.sex == "Женский":
+                gender = lists["sex"]
+                if gender == 1 and lists["online"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif self.sex == "Мужской":
+                gender = lists["sex"]
+                if gender == 2 and lists["online"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif lists["online"] == 1:
+                self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+       
+        except Exception as error:
+            print(error)
+            
+    
+    #---------------------------------------------------------------------------------------------
+    def open_dial(self, lists):    # Метод поиска пользователей с открытыми сообщениями
+        try: 
+            if self.sex == "Женский":
+                gender = lists["sex"]
+                if gender == 1 and lists["can_write_private_message"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif self.sex == "Мужской":
+                gender = lists["sex"]
+                if gender == 2 and lists["can_write_private_message"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif lists["can_write_private_message"] == 1:
+                self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+       
+        except Exception as error:
+            print(error)
+ 
+ 
+    #---------------------------------------------------------------------------------------------
+    def on_open_dial(self, lists):    # Метод поиска пользователей сообщества онлайн с открытыми сообщениями
+        try: 
+            if self.sex == "Женский":
+                gender = lists["sex"]
+                if gender == 1 and lists["can_write_private_message"] == 1 and lists["online"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif self.sex == "Мужской":
+                gender = lists["sex"]
+                if gender == 2 and lists["can_write_private_message"] == 1 and lists["online"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+            elif lists["can_write_private_message"] == 1 and lists["online"] == 1:
+                self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+       
+        except Exception as error:
+            print(error)
+ 
+
+    #---------------------------------------------------------------------------------------------
+    def on_old(self, lists):    # Метод поиска пользователей сообщества  по возрасту и онлайн
+        try: 
+            add_date = str(lists["bdate"])
+
+            if len(add_date) >= 4 and add_date[-4:].isdigit():
+                rez = add_date[-4:]
+                
+                if self.sex == "Женский":
+                    gender = lists["sex"]
+                    if gender == 1 and int(2021 - int(rez)) == self.stolder and lists["online"] == 1:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif self.sex == "Мужской":
+                    gender = lists["sex"]
+                    if gender == 2 and int(2021 - int(rez)) == self.stolder and lists["online"] == 1:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif int(2021 - int(rez)) == self.stolder and lists["online"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+   
+        except Exception as error:
+            print(error)
+
+
+    #---------------------------------------------------------------------------------------------
+    def old_dial(self, lists):    # Метод поиска пользователей сообщества  по возрасту и открытым диалогам
+        try: 
+            add_date = str(lists["bdate"])
+
+            if len(add_date) >= 4 and add_date[-4:].isdigit():
+                rez = add_date[-4:]
+                
+                if self.sex == "Женский":
+                    gender = lists["sex"]
+                    if gender == 1 and int(2021 - int(rez)) == self.stolder and lists["can_write_private_message"] == 1:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif self.sex == "Мужской":
+                    gender = lists["sex"]
+                    if gender == 2 and int(2021 - int(rez)) == self.stolder and lists["can_write_private_message"] == 1:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif int(2021 - int(rez)) == self.stolder and lists["can_write_private_message"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+   
+        except Exception as error:
+            print(error)
+
+
+    #---------------------------------------------------------------------------------------------
+    def on_old_open_dial(self, lists):    # Метод поиска пользователей сообщества  по всем  параметрам
+        try: 
+            add_date = str(lists["bdate"])
+
+            if len(add_date) >= 4 and add_date[-4:].isdigit():
+                rez = add_date[-4:]
+                
+                if self.sex == "Женский":
+                    gender = lists["sex"]
+                    if gender == 1 and int(2021 - int(rez)) == self.stolder and lists["can_write_private_message"] == 1 and lists["online"] == 1:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif self.sex == "Мужской":
+                    gender = lists["sex"]
+                    if gender == 2 and int(2021 - int(rez)) == self.stolder and lists["can_write_private_message"] == 1 and lists["online"] == 1:
+                        self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+            
+                elif int(2021 - int(rez)) == self.stolder and lists["can_write_private_message"] == 1 and lists["online"] == 1:
+                    self.length.emit(str(lists["id"]), str(lists["first_name"]), str(lists["last_name"]), str(lists["bdate"]), str(lists["online"]), str(lists["can_write_private_message"]))
+   
+        except Exception as error:
+            print(error)
+
+ 
